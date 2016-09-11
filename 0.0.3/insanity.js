3e9ef229b63863b1181989a733682e5e6b568617
WarrenHood <nullbyte001@gmail.com>
2016-09-11 17:02:10 +0200

    Add files via upload

--- 0.0.3/insanity.js
+++ 0.0.3/insanity.js
@@ -1,9 +1,10 @@
-localStorage.level = localStorage.level || 1;
+localStorage.level3 = localStorage.level3 || 1;
 numBlocks = 9;
-if(!localStorage.played)localStorage.level = 1;
-level = localStorage.level;
-version = '0.0.2';
-alert('Insanity Normal Mode\n\nHow to Play\n\nTap the red blocks to change their colour. If you tap a non-red block it will become red.The aim of the game is to eliminate all red blocks. The blocks will constantly change positions, so be careful.');
+mode = 'static'
+if(!localStorage.played)localStorage.level3 = 1;
+level3 = localStorage.level3;
+version = '0.0.3';
+alert('Insanity Puzzle Mode(Static)\n\nHow to Play\n\nTap the gray blocks to invert the colour of everything in its row or column. Tapping any inner block will result in all blocks in the colum and row of the tapped block. The aim of the game is to eliminate all red blocks.');
 swapInterval = 500;
 if (!String.prototype.includes) {
   String.prototype.includes = function(search, start) {
@@ -24,10 +25,13 @@ function gbtname(x){return document.getElementsByTagName(x);}
 function animate(){
 	var counter = 0;
 	levCompletionCheck();
-	var b1 = Math.floor(Math.random()*numBlocks);
-	var b2 = Math.floor(Math.random()*numBlocks);
-	while(((b2 == b1) || colAt(b1) == colAt(b2)) && counter < 10 ){b1 =Math.floor(Math.random()*numBlocks);counter++}
-	if(counter < 10)swap(b1,b2);
+	row = 1 + Math.floor(Math.random()*(Math.sqrt(numBlocks)-2));
+	col = 1 + Math.floor(Math.random()*(Math.sqrt(numBlocks)-2));
+		if(mode == 'dynamic'){
+	var b1 = blockNum(row,col);
+	var b2 = blockNum(row,col);
+	while(((b2 == b1) || colAt(b1) == colAt(b2)) && counter < 10 ){b1 = blockNum(1 + Math.floor(Math.random()*(Math.sqrt(numBlocks)-2)),1 + Math.floor(Math.random()*(Math.sqrt(numBlocks)-2)));counter++;}
+	if(counter < 100)swap(b1,b2);}
 	setTimeout(animate,swapInterval);
 }
 window.onload = function(){
@@ -37,7 +41,7 @@ window.onload = function(){
 	var size = screen.width;
 	if(size > screen.height)size = screen.height;
 	size *= 0.8;
-	lev(level-1);
+	lev(level3-1);
 	name = gbid('name').innerHTML;
 	text = '';
 	cLet = 0;
@@ -62,7 +66,7 @@ function nameAnim(){
 }
 function lev(n){
 	var blocks = gbid('grid').getElementsByTagName('td');
-	var levs = [[3,400],[3,300],[4,200],[4,150],[5,100],[5,80],[6,50],[6,40],[7,30],[7,20],[8,15],[8,10],[8,5],[8,1] ];
+	var levs = [[3,4000],[3,3000],[4,2000],[4,1500],[5,1000],[5,800],[6,500],[6,400],[7,300],[7,200],[8,150],[8,100],[8,50],[8,10] ];
 	if(n >= levs.length){alert('Congratulations! You have completed the game!');n = levs.length-1;
 	if(confirm("Restart from level 1?")){localStorage.level = 1;level = 1; n = 0;}
 	}
@@ -71,18 +75,32 @@ function lev(n){
 	swapInterval = levs[n][1];
 	var size = screen.width;
 	if(size > screen.height)size = screen.height;
-	size = Math.floor(size *0.7 / levs[n][0]);
-	setGrids(levs[n][0],size);
+	size = Math.floor((size *0.7) / (levs[n][0])+2);
+	setGrids(levs[n][0]+2,size);
 	for(var i = 0; i < Math.ceil(numBlocks/2);i++)setCol(i,'red');
 	for(var i = 0;i < numBlocks;i++)blocks[i].onclick = check;
+	setCol(blockNum(0,0),'black');
+	setCol(blockNum(0,rows-1),'black');
+	setCol(blockNum(rows-1,0),'black');
+	setCol(blockNum(rows-1,rows-1),'black');
+	startRow = 1;
+	endRow = rows-1;
+	for(var r = startRow;r<endRow;r++){
+		setCol(blockNum(r,0),'grey');
+		setCol(blockNum(0,r),'grey');
+		setCol(blockNum(r,endRow),'grey');
+		setCol(blockNum(endRow,r),'grey');
+		
+	}
 	shuffle();
 }
 function setGrids(n,s){
 	numBlocks = Math.pow(n,2);
+	rows = n;
 	var elt ='';
 	for(var i = 0; i < n;i++ ){
 		elt += '<tr>';
-		for(var j =0;j<n;j++)elt += '<td style="height:'+s+'px;width:'+s+'px;background:'+randColX()+'"></td>';
+		for(var j =0;j<n;j++)elt += '<td id="'+blockNum(i,j)+'" style="height:'+s+'px;width:'+s+'px;background:'+randColX()+'"></td>';
 	elt += '</tr>';
 	}
 	grid.innerHTML = elt;
@@ -110,15 +128,48 @@ function colAt(x){
 function check(e){
 	e = e || event || window.event;
 	var target = e.target || e.srcElement;
-	if(target.style.background == "red" || extractCol(target.style.background) == 'red' )target.style.background = randColX();
-	else target.style.background = "red";
+	for(var i=0;i<numBlocks;i++){
+		if(document.getElementById(i) == target)currentBlock = i;
+	}
+	direction = null;
+	if(rowNum(currentBlock) == 0 || rowNum(currentBlock) == rows-1)direction = 'v';
+	else if(colNum(currentBlock) == 0 || colNum(currentBlock) == rows-1)direction = 'h';
+	else direction = 'both'
+	if(direction == 'v'){
+		startRow = 1;
+		endRow = rows-1;
+		for(var r = startRow;r<endRow;r++){
+			if(colAt(blockNum(r,colNum(currentBlock))) == "red" || extractCol(colAt(blockNum(r,colNum(currentBlock)))) == 'red' )setCol(blockNum(r,colNum(currentBlock)),randColX());
+			else setCol(blockNum(r,colNum(currentBlock)),"red");
+		}
+	}
+	if(direction == 'h'){
+		startRow = 1;
+		endRow = rows-1;
+		for(var r = startRow;r<endRow;r++){
+			if(colAt(blockNum(rowNum(currentBlock),r)) == "red" || extractCol(colAt(blockNum(rowNum(currentBlock),r))) == 'red' )setCol(blockNum(rowNum(currentBlock),r),randColX());
+			else setCol(blockNum(rowNum(currentBlock),r),"red");
+		}
+	}
+	if(direction == 'both'){
+		for(var r = startRow;r<endRow;r++){	
+			if(colAt(blockNum(rowNum(currentBlock),r)) == "red" || extractCol(colAt(blockNum(rowNum(currentBlock),r))) == 'red' )setCol(blockNum(rowNum(currentBlock),r),randColX());
+			else setCol(blockNum(rowNum(currentBlock),r),"red");
+			if(colAt(blockNum(r,colNum(currentBlock))) == "red" || extractCol(colAt(blockNum(r,colNum(currentBlock)))) == 'red' )setCol(blockNum(r,colNum(currentBlock)),randColX());
+			else setCol(blockNum(r,colNum(currentBlock)),"red");
+		}
+		if(colAt(blockNum(rowNum(currentBlock),colNum(currentBlock))) == "red" || extractCol(colAt(blockNum(rowNum(currentBlock),colNum(currentBlock)))) == 'red' )setCol(blockNum(rowNum(currentBlock),colNum(currentBlock)),randColX());
+			else setCol(blockNum(rowNum(currentBlock),colNum(currentBlock)),"red");
+	}
+	//if(target.style.background == "red" || extractCol(target.style.background) == 'red' )target.style.background = randColX();
+	//else target.style.background = "red";
 }
 function levCompletionCheck(){
 	for(var i = 0; i < numBlocks;i++){if(colAt(i) == 'red' )return;}
-	alert('Level '+ level+ ' complete');
-	localStorage.level++;
-	level = localStorage.level;
-	lev(level-1);
+	alert('Level '+ level3+ ' complete');
+	localStorage.level3++;
+	level3 = localStorage.level3;
+	lev(level3-1);
 }
 colors = ['red','orange','yellow','green','blue','purple','pink'];
 function extractCol(c){

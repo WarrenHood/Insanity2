c680ec1a9eadbcfdb25a1e7200eac88e221d7f6f
WarrenHood <nullbyte001@gmail.com>
2016-09-22 15:59:28 +0200

    Add files via upload

--- 0.0.3/insanity.js
+++ 0.0.3/insanity.js
@@ -4,7 +4,7 @@ numBlocks = 9;
 mode = 'static';
 if(!localStorage.played)localStorage.level3 = 1;
 level3 = localStorage.level3;
-version = '0.0.3';
+version = '0.0.4';
 //alert('Insanity Puzzle Mode(Static)\n\nHow to Play\n\nTap the gray blocks to invert the colour of everything in its row or column. Tapping any inner block will result in all blocks in the colum and row of the tapped block. The aim of the game is to eliminate all red blocks.');
 swapInterval = 500;
 function gbid(x){return document.getElementById(x);}
